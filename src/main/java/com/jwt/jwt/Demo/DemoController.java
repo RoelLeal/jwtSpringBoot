14426d54c3e0a4657d7c5b4478706699e49b9f08
RoelLeal <vegerex91@gmail.com>
2023-12-20 19:46:44 -0600

    FIRST COMMIT

---
+++ src/main/java/com/jwt/jwt/Demo/DemoController.java
@@ -0,0 +1,18 @@
+package com.jwt.jwt.Demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class DemoController {
+    
+    @PostMapping(value="demo")
+    public String demo() {
+        return "Bienvenido papi";
+    }
+
+}
---
+++ src/main/java/com/jwt/jwt/User/Role.java
@@ -0,0 +1,6 @@
+package com.jwt.jwt.User;
+
+public enum Role {
+    ADMIN,
+    USER
+}
